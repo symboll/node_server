2b5c632d6d4724b13bcb08e8a6ee11ee777cc6b8
likanghua <lkh19890913@163.com>
2020-11-06 09:13:14 +0800

    feat: add ejs

--- basic/bin/www.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const serverHandle = require('../app')
-
-const PORT = 80
-const server = http.createServer(serverHandle)
-
-server.listen(PORT, () => {
-  console.log('http://localhost:80')
-})

08d9b9c5fe6c508d82a781fb5aac2a573b39b707
likanghua <lkh19890913@163.com>
2021-01-13 22:46:26 +0800

    feat: 增加跨域

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -7,7 +7,7 @@ const port = 3000
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors()
   app.useGlobalPipes(new ValidationPipe() )
 
   const options = new DocumentBuilder()
